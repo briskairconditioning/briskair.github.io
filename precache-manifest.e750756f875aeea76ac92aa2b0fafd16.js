1c3fc5e7442f64c0ad0b348aa085b6db820b2115
briskairconditioning <hellobriskair@gmail.com>
2025-02-02 12:23:06 +0530

    changes

---
+++ precache-manifest.e750756f875aeea76ac92aa2b0fafd16.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f93ab7c67bced5135db2f904d4649c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1196b5d61d5d541dc81",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6c2813644e7da31653d0",
+    "url": "/static/js/2.57d48844.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.57d48844.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7aaa515cd36a130ebcd",
+    "url": "/static/js/3.bbd7c9fd.chunk.js"
+  },
+  {
+    "revision": "d1196b5d61d5d541dc81",
+    "url": "/static/js/main.a2b626a8.chunk.js"
+  },
+  {
+    "revision": "b33f3c5412214188b89b",
+    "url": "/static/js/runtime-main.a0d5be1a.js"
+  },
+  {
+    "revision": "857d2bf7fb98e3d6443cecb484cf6605",
+    "url": "/static/media/bosch.857d2bf7.svg"
+  },
+  {
+    "revision": "0d05150cf529320af5cbbebd83627e32",
+    "url": "/static/media/briskair.0d05150c.svg"
+  },
+  {
+    "revision": "39b9ce590ec7cd22645dd80311a9e2b7",
+    "url": "/static/media/ifb.39b9ce59.svg"
+  },
+  {
+    "revision": "cee83ef76246804fdfd4912017052ab2",
+    "url": "/static/media/newest.cee83ef7.svg"
+  },
+  {
+    "revision": "4dca4ffec6d442ab4752d6ef9450d92c",
+    "url": "/static/media/profileReplace.4dca4ffe.png"
+  },
+  {
+    "revision": "ff9c74d9eec67b11a8caf5d1bd997657",
+    "url": "/static/media/samsung.ff9c74d9.svg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "aceeb6edb29555875e13627a6ad4ba0e",
+    "url": "/static/media/svg-2-replace.aceeb6ed.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "8daa865ffe72365597b88f565771616e",
+    "url": "/static/media/welcome1.8daa865f.svg"
+  },
+  {
+    "revision": "f05db8119e9d196419ea0b4c942f6d47",
+    "url": "/static/media/westinghouse.f05db811.svg"
+  },
+  {
+    "revision": "ba90dc77fb82574fa4e224e804df95aa",
+    "url": "/static/media/whatsapp.ba90dc77.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2eab5103c405d29a897811f4661a2ff6.js"
+  "/precache-manifest.e750756f875aeea76ac92aa2b0fafd16.js"
 );
 
 self.addEventListener('message', (event) => {
