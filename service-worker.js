5d0f0aeee1a660862b7984337773413b1d2b4e2c
briskairconditioning <hellobriskair@gmail.com>
2025-02-02 11:28:24 +0530

    changes

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.494c711e8bfadd56a405f5b655fdd986.js"
+  "/precache-manifest.2eab5103c405d29a897811f4661a2ff6.js"
 );
 
 self.addEventListener('message', (event) => {
